27ee7feec556b99bed62d9cc0253fcbf2c2fa107
Rafael Martinez <rgmllc@yahoo.com>
2023-04-12 20:49:52 +0000

    creating functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+"""
+Functions
+
+The function is defined using def.
+The function has a name hello_world.
+The next line is indented to show it is inside the function.
+The function is called by last line hello_world()
+
+
+"""
+
+# A function that prints hello world
+def hello_world():
+    print('hello world')
+
+# This line calls (runs) the function
+hello_world()
+
+
+# Assign the hello_world() function to a variable.
+greeting = hello_world()
+print(greeting)
+
