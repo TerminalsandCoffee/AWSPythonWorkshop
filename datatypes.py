f374c1d64d6fed116c81d3a1c18c265766cf0a63
Rafael Martinez <rgmllc@yahoo.com>
2023-04-12 00:42:00 +0000

    completed the data types section of the workshop

--- datatypes.py
+++ datatypes.py
@@ -6,3 +6,71 @@ sentence = "The total comes to: "
 
 print(sentence + str(my_int))
 
+#str() returns a string object
+#int() returns an integer object
+#float() returns a floating point object
+#bool() a boolean value of True or False
+
+
+
+
+
+
+
+
+"""
+        dictionaries
+To read the value associated with a key, 
+you need to provide the name of the dictionary and the the value of the key inside square brackets.
+"""
+user = {"first_name":"Ada"}
+print(user["first_name"])
+#output: Ada
+
+"""
+to update
+Dictionaries are mutable, which means they can be changed after you create them. 
+You can add, update or delete the key-value pairs in a dictionary.
+"""
+
+user["family_name"] = "Byron"
+print(user)
+#output {'first_name': 'Ada', 'family_name': 'Byron'}
+
+
+#modify
+user["family_name"] = "Lovelace"
+print(user)
+# {'first_name': 'Ada', 'family_name': 'Lovelace'}
+
+
+#delete
+del user["family_name"]
+print(user)
+#output {'first_name': 'Ada'}
+
+
+
+"""
+To determine a type
+"""
+
+my_variable = "A string"
+print(type(my_variable))
+#output <class 'str'>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
